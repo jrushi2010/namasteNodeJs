52913123f03d80d49613b53f319e2451ade51e53
jrushi2010 <jrushi2010@gmail.com>
2024-09-20 12:19:53 +0530

    created middleware auth.js for jwt token validation

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const { validateSignUpData } = require('./utils/validation');
 const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
+const { userAuth } = require('./middlewares/auth');
 
 app.use(express.json());
 app.use(cookieParser());
@@ -67,24 +68,11 @@ app.post("/login", async (req, res) => {
     }
 });
 
-app.get("/profile", async (req, res) => {
+app.get("/profile", userAuth, async (req, res) => {
 
     try {
-        const cookies = req.cookies;
 
-        const { token } = cookies;
-        //validate my token 
-        const decodedMessage = await jwt.verify(token, "DEV@Tinder$790");
-
-        const { _id } = decodedMessage;
-
-        console.log("Logged in user is: " + _id);
-
-        const user = await User.findById(_id);
-
-        if (!user) {
-            throw new Error("User does not exists...");
-        }
+        const user = req.user;
 
         res.send(user);
     } catch (err) {
